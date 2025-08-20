59975b92221823a17971abe935b28407e310ebfa
YazanLpizra <YazanLpizra@users.noreply.github.com>
2025-08-20 15:53:35 -0400

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+    site: "https://www.yourwebsite.com", // update me!
+    integrations: [
+        icon(),
+        sitemap(),
+    ],
+});
