ce7c80aa6d3d214c41ddbddf37f790d3e100efc5
Yazan Lpizra <ylpizra@my.bridgeport.edu>
2025-08-26 15:08:37 -0400

    Refactor layout components and optimize images
    - Replaced BaseLayout with InteriorPageLayout in contact.astro for better structure.
    - Moved CTA component to common directory and updated import paths.
    - Added new Footer and Header components for consistent layout.
    - Created Landing component for better image handling and title display.
    - Introduced InteriorPageLayout to manage landing images and descriptions.
    - Updated styles for responsiveness and dark mode compatibility.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,5 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
     site: "https://www.yourwebsite.com", // update me!
-    integrations: [
-        icon(),
-        sitemap(),
-    ],
+    integrations: [icon(), sitemap()],
 });
